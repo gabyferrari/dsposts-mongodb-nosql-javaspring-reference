d642002de0e495cc9dcb734e3175b6b061b5d017
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-15 16:35:23 -0300

    GET user by id

--- src/main/java/com/gabyferrari/dsposts/services/UserService.java
+++ src/main/java/com/gabyferrari/dsposts/services/UserService.java
@@ -1,6 +1,7 @@
 package com.gabyferrari.dsposts.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.gabyferrari.dsposts.models.dto.UserDTO;
 import com.gabyferrari.dsposts.models.entities.User;
 import com.gabyferrari.dsposts.repositories.UserRepository;
+import com.gabyferrari.dsposts.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -19,5 +21,11 @@ public class UserService {
 	public List<UserDTO> findAll() {
 		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+	
+	public UserDTO findById(String id) {
+		Optional<User> result = repository.findById(id);
+		User entity = result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+		return new UserDTO(entity);
 	}
 }
