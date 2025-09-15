5bf1bf3f9356724ea231f00aaa8136e6d497e1c6
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-15 12:17:47 -0300

    Project created

---
+++ src/main/java/com/gabyferrari/dsposts/DspostsMongodbNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.gabyferrari.dsposts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspostsMongodbNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspostsMongodbNosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabyferrari/dsposts/DspostsMongodbNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabyferrari.dsposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspostsMongodbNosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
